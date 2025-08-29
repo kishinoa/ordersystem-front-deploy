1876a63b585ef144fa4799005e8e086b995fab20
kishinoa <chltmdgnl11@gmail.com>
2025-08-29 11:07:12 +0900

    fix

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -12,7 +12,7 @@
                     </div>
                 </v-col>
                 <v-col class="text-center">
-                    <v-btn :to="'/'">devops store test/v-btn>
+                    <v-btn :to="'/'">devops store test</v-btn>
                 </v-col>
                 <v-col class="d-flex justify-end">
                     <v-btn v-if="isLogined" :to="'/order/cart'">장바구니{{ totalQuantity }}</v-btn>
